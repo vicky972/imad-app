250132c7aa9bfea781cbcec3672b9ed88b99f5f8
vicky972 <vickymishra972@gmail.com>
2018-03-08 19:33:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,18 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/article 1', function(req,res){
+    res.send('article 1 is served here');
+});
+app.get('/article 2', function(req,res){
+    res.send('article 2 is served here');
+});
+app.get('/article 3', function(req,res){
+    res.send('article 3 is served here');
+});
+app.get('/article 4', function(req,res){
+    res.send('article 4 is served here');
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
